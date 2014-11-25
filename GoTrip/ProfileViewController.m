741091d85745120a64d4a3c46be3ff873a78afdd
Andrew Liu <dieseliu321@gmail.com>
2014-11-25 09:03:10 -0800

    save profile

--- GoTrip/ProfileViewController.m
+++ GoTrip/ProfileViewController.m
@@ -16,14 +16,13 @@
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *locationLabel;
 @property (weak, nonatomic) IBOutlet UILabel *memoLabel;
-@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *firstNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *locationTextField;
 @property (weak, nonatomic) IBOutlet UITextField *memoTextField;
-@property (weak, nonatomic) IBOutlet UIButton *nameButton;
-@property (weak, nonatomic) IBOutlet UIButton *locationButton;
-@property (weak, nonatomic) IBOutlet UIButton *memoButton;
 @property (weak, nonatomic) IBOutlet UIButton *imageButton;
 @property Profile *profile;
+@property BOOL isImagePickerCalled;
 
 @end
 
@@ -32,6 +31,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    if (!self.isImagePickerCalled)
+    {
+        [self editMode:NO];
+        self.navigationItem.rightBarButtonItem.title = @"Edit";
+        [self refreshPersonalProfile];
+    }
+    else
+    {
+        self.isImagePickerCalled = NO;
+    }
+}
+
+- (void)refreshPersonalProfile
+{
     [Profile getCurrentProfileWithCompletion:^(Profile *profile, NSError *error)
     {
         if (!error)
@@ -41,6 +59,7 @@
             self.nameLabel.text = [NSString stringWithFormat:@"%@ %@", self.profile.firstName, self.profile.lastName];
             self.locationLabel.text = self.profile.locationName;
             self.memoLabel.text = self.profile.memo;
+            self.isImagePickerCalled = NO;
         }
         else
         {
@@ -48,24 +67,81 @@
         }
     }];
 }
-- (IBAction)editProfileOnButtonPressed:(UIBarButtonItem *)sender
+
+- (void)editMode:(BOOL)yes
 {
+    [self.nameLabel setHidden:yes];
+    [self.locationLabel setHidden:yes];
+    [self.memoLabel setHidden:yes];
+    [self.firstNameTextField setHidden:!yes];
+    [self.lastNameTextField setHidden:!yes];
+    [self.locationTextField setHidden:!yes];
+    [self.memoTextField setHidden:!yes];
+    [self.imageButton setHidden:!yes];
 }
 
+- (IBAction)editProfileOnButtonPressed:(UIBarButtonItem *)sender
+{
+    if ([sender.title isEqual: @"Edit"])
+    {
+        self.firstNameTextField.text = self.profile.firstName;
+        self.lastNameTextField.text = self.profile.lastName;
+        self.locationTextField.text = self.profile.locationName;
+        self.memoTextField.text = self.profile.memo;
+        [self editMode:YES];
+        sender.title = @"Save";
+    }
+    else
+    {
+        self.profile.firstName = self.firstNameTextField.text;
+        self.profile.lastName = self.lastNameTextField.text;
+        self.profile.locationName = self.locationTextField.text;
+        self.profile.memo = self.memoTextField.text;
+        [self.profile saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error)
+        {
+            if (!error)
+            {
+                [self editMode:NO];
+                sender.title = @"Edit";
+                [self refreshPersonalProfile];
+            }
+            else
+            {
+                [self error:error];
+            }
+        }];
+    }
+}
+
+//MARK: triger UIImagePicker(PhotoLibrary) by button pressed
 - (IBAction)changeImageOnButtonPressed:(UIButton *)sender
 {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    self.isImagePickerCalled = YES;
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentViewController:picker animated:YES completion:nil];
+}
 
+//MARK: UIImagePicker delegate to store and SAVE profile.avatarData and display on imageview
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *pickerImage = info[UIImagePickerControllerEditedImage];
+    self.profile.avatarData = UIImageJPEGRepresentation(pickerImage, 0.1);
+    self.imageView.image = pickerImage;
+    [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
 //MARK: UIAlert
 - (void)error:(NSError *)error
 {
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error"
-                                                               message:error.localizedDescription
-                                                        preferredStyle:UIAlertControllerStyleAlert];
+                                                                   message:error.localizedDescription
+                                                            preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK"
-                                                 style:UIAlertActionStyleDefault
-                                               handler:nil];
+                                                     style:UIAlertActionStyleDefault
+                                                   handler:nil];
     [alert addAction:action];
     [self presentViewController:alert animated:YES completion:nil];
 }
