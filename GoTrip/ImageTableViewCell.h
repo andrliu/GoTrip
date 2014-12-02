f85ebe5e306a01fcbb4995872d5540e415061847
Alexey Emelyanov <emelyanoff@gmail.com>
2014-12-01 17:53:41 -0800

    super-duper cool, tableview+collectionview

---
+++ GoTrip/ImageTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  ImageTableViewCell.h
+//  GoTrip
+//
+//  Created by Alex on 12/1/14.
+//  Copyright (c) 2014 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageTableViewCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
