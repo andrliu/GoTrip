f3d677048c4d34d28c166637605d02c1cc79cb4d
Jonathan Chou <jonathanchou@Jonathans-MacBook-Pro-2.local>
2014-12-01 20:26:27 -0800

    readded test vc

--- GoTrip/MessagesViewController.m
+++ GoTrip/MessagesViewController.m
@@ -10,7 +10,7 @@
 #import "Message.h"
 #import "Profile.h"
 #import "ChatViewController.h"
-//#import "TestViewController.h"
+#import "TestViewController.h"
 
 @interface MessagesViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -70,8 +70,8 @@
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // TestViewController *testVC = [[TestViewController alloc] init];
-    //  [self.navigationController pushViewController:testVC animated:YES];
+     TestViewController *testVC = [[TestViewController alloc] init];
+      [self.navigationController pushViewController:testVC animated:YES];
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
---
+++ GoTrip/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  GoTrip
+//
+//  Created by Jonathan Chou on 12/1/14.
+//  Copyright (c) 2014 Andrew Liu. All rights reserved.
+//
+
+#import "JSQMessagesViewController.h"
+
+@interface TestViewController : JSQMessagesViewController
+
+@end
