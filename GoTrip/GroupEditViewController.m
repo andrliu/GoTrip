92f5f2f7b5a05e0a0b195be1a3e1f327b1891b95
Alexey Emelyanov <emelyanoff@gmail.com>
2014-12-03 23:23:08 -0800

    added some cool animation, unwidnd segue when delete a group

--- GoTrip/GroupEditViewController.m
+++ GoTrip/GroupEditViewController.m
@@ -32,6 +32,11 @@
 //   }];
 }
 
+- (IBAction)onDeleteButtonPressed:(UIBarButtonItem *)sender
+{
+    [self deleteGroupAlertWindow];
+}
+
 -(BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender
 {
     if (self.group.objectId)
@@ -44,5 +49,47 @@
         return YES;
     }
 }
+
+//deletes the group and jumps to the homeViewController
+- (void)deleteGroupAlertWindow
+{
+    NSString *warningMessage = [NSString stringWithFormat:@"You are about to delete the group. \nDelete?"]; //add variable to the warning message
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Warning"
+                                                                   message:warningMessage
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *confirm = [UIAlertAction actionWithTitle:@"Yes"
+                                                      style:UIAlertActionStyleDefault
+                                                    handler:^(UIAlertAction *action)
+                                                    {
+                                                        [self.group deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error)
+                                                        {
+                                                            if (error)
+                                                            {
+                                                                [self errorAlertWindow:error.localizedDescription];
+                                                            }
+                                                            else
+                                                            {
+                                                                [self performSegueWithIdentifier:@"goHomeSegue" sender:nil];
+                                                            }
+                                                        }];
+
+
+                                                    }];
+    UIAlertAction *cancell = [UIAlertAction actionWithTitle:@"NO!"
+                                                     style:UIAlertActionStyleDestructive
+                                                   handler:nil];
+    [alert addAction:confirm];
+    [alert addAction:cancell];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+-(void)errorAlertWindow:(NSString *)message
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error!" message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okButton = [UIAlertAction actionWithTitle:@"😭 OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:okButton];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
 
 @end
