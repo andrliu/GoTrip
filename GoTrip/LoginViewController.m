0843e8392150e3331420195f342d228d421b07ee
Andrew Liu <dieseliu321@gmail.com>
2014-12-04 19:08:35 -0800

    update storyboard

--- GoTrip/LoginViewController.m
+++ GoTrip/LoginViewController.m
@@ -24,29 +24,30 @@
 - (void)viewDidLayoutSubviews
 {
     [super viewDidLayoutSubviews];
+    
     [self.logInView.logo setFrame:CGRectMake(self.logInView.frame.size.width/6,
                                              self.logInView.frame.size.height/8,
                                              self.logInView.frame.size.width*2/3,
                                              self.logInView.frame.size.width*2/9)];
-    [self.logInView.usernameField setFrame:CGRectMake(self.logInView.frame.size.width/6,
+    [self.logInView.usernameField setFrame:CGRectMake(-5.0f,
                                                       self.logInView.frame.size.height*3/8 - 5.0f,
-                                                      self.logInView.frame.size.width*2/3,
+                                                      self.logInView.frame.size.width + 10.0f,
                                                       self.logInView.frame.size.height/12)];
-    [self.logInView.passwordField setFrame:CGRectMake(self.logInView.frame.size.width/6,
+    [self.logInView.passwordField setFrame:CGRectMake(-5.0f,
                                                       self.logInView.frame.size.height*11/24,
-                                                      self.logInView.frame.size.width*2/3,
+                                                      self.logInView.frame.size.width + 10.0f,
                                                       self.logInView.frame.size.height/12)];
-    [self.logInView.logInButton setFrame:CGRectMake(self.logInView.frame.size.width/6,
+    [self.logInView.logInButton setFrame:CGRectMake(-5.0f,
                                                     self.logInView.frame.size.height*13/24 + 5.0f,
-                                                    self.logInView.frame.size.width*2/3,
+                                                    self.logInView.frame.size.width + 10.0f,
                                                     self.logInView.frame.size.height/12)];
-    [self.logInView.facebookButton setFrame:CGRectMake(self.logInView.frame.size.width/6,
+    [self.logInView.facebookButton setFrame:CGRectMake(-5.0f,
                                                        self.logInView.frame.size.height*33/48 + 2.5f,
-                                                       self.logInView.frame.size.width*2/3,
+                                                       self.logInView.frame.size.width + 10.0f,
                                                        self.logInView.frame.size.height/12)];
-    [self.logInView.signUpButton setFrame:CGRectMake(self.logInView.frame.size.width/6,
+    [self.logInView.signUpButton setFrame:CGRectMake(-5.0f,
                                                      self.logInView.frame.size.height*5/6,
-                                                     self.logInView.frame.size.width*2/3,
+                                                     self.logInView.frame.size.width + 10.0f,
                                                      self.logInView.frame.size.height/12)];
 }
 
--- GoTrip/SignupViewController.m
+++ GoTrip/SignupViewController.m
@@ -27,18 +27,18 @@
                                               self.signUpView.frame.size.height/8,
                                               self.signUpView.frame.size.width*2/3,
                                               self.signUpView.frame.size.width*2/9)];
-    [self.signUpView.usernameField setFrame:CGRectMake(self.signUpView.frame.size.width/6,
+    [self.signUpView.usernameField setFrame:CGRectMake(-5.0f,
                                                        self.signUpView.frame.size.height*3/8 - 5.0f,
-                                                       self.signUpView.frame.size.width*2/3,
+                                                       self.signUpView.frame.size.width + 10.0f,
                                                        self.signUpView.frame.size.height/12)];
-    [self.signUpView.passwordField setFrame:CGRectMake(self.signUpView.frame.size.width/6,
+    [self.signUpView.passwordField setFrame:CGRectMake(-5.0f,
                                                        self.signUpView.frame.size.height*11/24,
-                                                       self.signUpView.frame.size.width*2/3,
+                                                       self.signUpView.frame.size.width + 10.0f,
                                                        self.signUpView.frame.size.height/12)];
-    [self.signUpView.signUpButton setFrame:CGRectMake(self.signUpView.frame.size.width/6,
-                                                     self.signUpView.frame.size.height*13/24 + 5.0f,
-                                                     self.signUpView.frame.size.width*2/3,
-                                                     self.signUpView.frame.size.height/12)];
+    [self.signUpView.signUpButton setFrame:CGRectMake(-5.0f,
+                                                      self.signUpView.frame.size.height*13/24 + 5.0f,
+                                                      self.signUpView.frame.size.width + 10.0f,
+                                                      self.signUpView.frame.size.height/12)];
 }
 
 @end
